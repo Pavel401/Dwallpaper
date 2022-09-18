df2a22e0cc8091855092613f6f3e08e0c6b5751d
Pavel401 <pavelalam401@gmail.com>
2022-09-18 11:16:58 +0530

    Carosole OnCllick done

--- lib/UI/Homepage.dart
+++ lib/UI/Homepage.dart
@@ -290,18 +290,37 @@ class WallpaperCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      imageUrl: controller.carosoleItems[i]['src']['landscape'],
-      imageBuilder: (context, imageProvider) => Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
+    return InkWell(
+      onTap: () {
+        Get.to(
+          () => ImageView(
+            id: controller.carosoleItems[i]['id'],
+            photographer:
+                controller.carosoleItems[i]['photographer'].toString(),
+            photographer_url:
+                controller.carosoleItems[i]['photographer_url'].toString(),
+            photographer_id: controller.carosoleItems[i]['photographer_id'],
+            large2x: controller.carosoleItems[i]['src']['portrait'].toString(),
+            large: controller.carosoleItems[i]['src']['portrait'].toString(),
+            width: controller.carosoleItems[i]['width'].toString(),
+            height: controller.carosoleItems[i]['height'].toString(),
+            avg_color: controller.carosoleItems[i]['avg_color'].toString(),
+          ),
+        );
+      },
+      child: CachedNetworkImage(
+        imageUrl: controller.carosoleItems[i]['src']['landscape'],
+        imageBuilder: (context, imageProvider) => Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
               image: imageProvider,
               fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(Colors.red, BlendMode.colorBurn)),
+            ),
+          ),
         ),
+        errorWidget: (context, url, error) => Icon(Icons.error),
       ),
-      errorWidget: (context, url, error) => Icon(Icons.error),
     );
   }
 }
